e123faa8fc037bef6d90ff8ea6b6acd4feefd753
Professor Sahil <sahil.malume@gmail.com>
2026-04-19 23:40:37 +0200

    Update config.js

--- config.js
+++ config.js
@@ -12,7 +12,7 @@ module.exports = {
   sessionID: process.env.SESSION_ID || '',
 
   // 🔗 SYSTEM LINKS
-  newsletterJid: '', // 📢 WhatsApp newsletter ID
+  newsletterJid: 'newsletterJid: '120363406672648713@newsletter',', // 📢 WhatsApp newsletter ID
   updateZipUrl: '',
 
   // 🎨 STICKER CONFIG
