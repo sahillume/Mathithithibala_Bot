9afc9ff700c148e972011f48f4bd7bac6a5ed73e
sahillume <sahil.malume@gmail.com>
2026-04-19 16:49:39 +0200

    Update waifu.js

--- commands/anime/waifu.js
+++ commands/anime/waifu.js
@@ -1,1 +1,30 @@
+/**
+ * Waifu Command - Random Anime Waifu
+ */
 
+const axios = require("axios");
+
+module.exports = {
+  name: "waifu",
+  aliases: ["waifupic"],
+  category: "anime",
+  description: "Get a random anime waifu image",
+  usage: ".waifu",
+
+  async execute(sock, msg, args, extra) {
+    try {
+      const chatId = msg.key.remoteJid;
+
+      const res = await axios.get("https://api.waifu.pics/sfw/waifu");
+
+      await sock.sendMessage(chatId, {
+        image: { url: res.data.url },
+        caption: "🌸 *Your Waifu*\n\n🤖 Mathithibala_Bot"
+      }, { quoted: msg });
+
+    } catch (err) {
+      console.log("WAIFU ERROR:", err);
+      extra.reply("❌ Failed to fetch waifu.");
+    }
+  }
+};
